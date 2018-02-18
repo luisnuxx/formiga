5056da30098dac91c07f8838295aa43647b9476d
Luis <luis.rodrigues@luisnuxx.com>
2018-02-18 17:16:04 +0000

    some ajustments done to CMakeLists.txt

---
+++ cmake-build-debug/CMakeFiles/formiga.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/luisnuxx/Development/C++/formiga/src/main.cpp" "/Users/luisnuxx/Development/C++/formiga/cmake-build-debug/CMakeFiles/formiga.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/boost/1.64.0_1/include"
+  "../src/include"
+  "../src/include/spdlog/include"
+  "../src/include/app"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
