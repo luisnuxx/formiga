aaeb636e6a0d983cf2311939c26b7b4dad4bc9e6
Luis <luis.rodrigues@luisnuxx.com>
2018-02-25 17:06:00 +0000

    travis build file

---
+++ tools/travis_build_before_script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir -p build # build folder
+mkdir -p build/www # build folder
+mkdir -p build/log # build folder
+
+cp CMakeLists.txt build  # required by cmake inside the build folder
