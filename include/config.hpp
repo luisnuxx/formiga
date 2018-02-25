3ed6bdb5638209a6c73ae7e4135cd6fb1509d334
Luis <luis.rodrigues@luisnuxx.com>
2018-02-25 22:38:17 +0000

    string_view issues

--- include/config.hpp
+++ include/config.hpp
@@ -5,7 +5,6 @@
 #ifndef FORMIGA_CONFIG_HPP
 #define FORMIGA_CONFIG_HPP
 #include <boost/iostreams/device/mapped_file.hpp>
-#include <experimental/string_view>
 #include <nlohmann/json.hpp>
 #include <iostream>
 #include <string>
